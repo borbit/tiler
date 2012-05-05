827678479c93fe25e13235f9842ac2ed27b43ae1
Serge Borbit <serge.borbit@gmail.com>
2012-05-06 01:06:25 +0300

    Header and license.

--- jquery.tiler.js
+++ jquery.tiler.js
@@ -1,30 +1,14 @@
-/*┌────────────────────────────────────────────────────────────────────────────┐
-  | jQuery widget to display infinite content as a grid of tiles               |
-  ├────────────────────────────────────────────────────────────────────────────┤
-  | License                                                                    |
-  |                                                                            |
-  | (The MIT License)                                                          |
-  |                                                                            |
-  | Copyright (c) 2011 Serge Borbit (serge.borbit@gmail.com)                   |
-  |                                                                            |
-  | Permission is hereby granted, free of charge, to any person obtaining a    |
-  | copy of this software and associated documentation files (the 'Software'), |
-  | to deal in the Software without restriction, including without limitation  |
-  | the rights to use, copy, modify, merge, publish, distribute, sublicense,   |
-  | and/or sell copies of the Software, and to permit persons to whom the      |
-  | Software is furnished to do so, subject to the following conditions:       |
-  |                                                                            |
-  | The above copyright notice and this permission notice shall be included    |
-  | in all copies or substantial portions of the Software.                     |
-  |                                                                            |
-  | THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS    |
-  | OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,|
-  | FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    |
-  | THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER |
-  | LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    |
-  | FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        |
-  | DEALINGS IN THE SOFTWARE.                                                  |
-  └────────────────────────────────────────────────────────────────────────────┘*/
+/*    
+  Tiler 0.1.0
+  
+  Library for the displaying of an infinite content
+  as a grid of tiles. For more info visit:
+  https://github.com/borbit/tiler/
+  
+  Copyright (c) 2011-2012 Serge Borbit <serge.borbit@gmail.com>
+  
+  Licensed under the MIT license
+*/
 
 (function($) {
 
